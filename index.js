58b8e73cd3035f5820d263efac263752fe4fbdf5
Atashi <atashi.khatua@speedboxapp.com>
2019-02-19 16:27:33 +0530

    Formatted files

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var express = require("express");
-var path    = require("path");
+var path = require("path");
 
-var app     = express();
+var app = express();
 
-app.use('/images', express.static(__dirname + '/public/images'));
-app.use('/data', express.static(__dirname + '/public/data'));
-app.use('/header', express.static(__dirname + '/public/pages/header.html'));
+app.use("/images", express.static(__dirname + "/public/images"));
+app.use("/data", express.static(__dirname + "/public/data"));
+app.use("/header", express.static(__dirname + "/public/pages/header.html"));
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get('/nearesthospital',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/pages/nearesthospital.html'));
+app.get("/nearesthospital", function(req, res) {
+	res.sendFile(path.join(__dirname + "/public/pages/nearesthospital.html"));
 });
 
-app.get('/tips',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/pages/tips.html'));
+app.get("/tips", function(req, res) {
+	res.sendFile(path.join(__dirname + "/public/pages/tips.html"));
 });
-
 
 app.listen(3001);
 
